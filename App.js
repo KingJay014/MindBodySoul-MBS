ef444c6dd3930404a8a8a255f9c31746d1f6218a
grotaatwit <grota@wit.edu>
2023-05-02 17:50:17 -0400

    added title fade-in on homepage

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Animated } from 'react-native';
+import React, {useEffect, useRef} from 'react';
+
+//IN ORDER TO RUN IN EXPO GO:
+//*
+//cd into project directory
+//npx expo start
+//scan barcode in app
 
 export default function App() {
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
   return (
-    <View style={styles.container}>
-      <Text>Mind, Body, and Soul</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Animated.Text style={[styles.text, {opacity: fadeAnim}]}>Mind, Body, and Soul</Animated.Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
+  text: {
+    fontSize: 34,
+    paddingTop: 100,
+  }
 });
