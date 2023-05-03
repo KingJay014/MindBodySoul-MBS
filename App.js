7607da46a4fef7d51f08da48a4562e702476919b
grotaatwit <grota@wit.edu>
2023-05-03 15:10:04 -0400

    added more menus within "mind section", added fading breathing text

--- App.js
+++ App.js
@@ -56,7 +56,7 @@ const HomePage = ({navigation}) => {
 
   return (
       <SafeAreaView style={styles.container}>
-        <View>
+        <View style={styles.centered}>
           <Animated.Text style={[styles.text, {opacity: fadeAnim, paddingBottom: 40}]}>Mind, Body, and Soul</Animated.Text>
 
           {/*
@@ -89,9 +89,118 @@ const HomePage = ({navigation}) => {
 //THE MIND PAGE
 const MindPage = ({navigation}) => {
 
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim2 = useRef(new Animated.Value(0)).current;
+  const fadeAnim3 = useRef(new Animated.Value(0)).current;
+  const fadeAnim4 = useRef(new Animated.Value(0)).current;
+  
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(fadeAnim2, {
+      delay: 100,
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(fadeAnim3, {
+      delay: 200,
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(fadeAnim4, {
+      delay: 300,
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim, fadeAnim2, fadeAnim3, fadeAnim4]);
+
+  return (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.centered}>
+    <TouchableOpacity style={{padding: 0, alignSelf: 'center' }} onPress={() => navigation.navigate('Breath')}>
+      <Animated.Image source={require('./assets/BreathButton.png')} style={[styles.buttonStyle, {opacity: fadeAnim2} ]} />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{padding: 0, alignSelf: 'center', opacity: fadeAnim3}} onPress={() => navigation.navigate('Extra Information')}>
+      <Animated.Image source={require('./assets/3DotsButton.png')} style={[styles.buttonStyle, {opacity: fadeAnim3}]} />
+      </TouchableOpacity>
+
+      {/*}
+      <TouchableOpacity style={{padding: 0, alignSelf: 'center', opacity: fadeAnim4}} onPress={() => navigation.navigate('Soul')}>
+      <Animated.Image source={require('./assets/SoulButton.png')} style={[styles.buttonStyle, {opacity: fadeAnim4}]} />
+      </TouchableOpacity>
+      */}
+    </View>
+  </SafeAreaView>
+  );
 }
 //END MIND PAGE
 
+const BreathingPage = ({navigation}) => {
+
+  const fadeAnim5 = useRef(new Animated.Value(0)).current;
+  const fadeAnim6 = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim5, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim5, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim5, {
+          toValue: 0,
+          duration: 6000,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start(),
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim6, {
+          toValue: 0,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim6, {
+          toValue: 1,
+          duration: 4500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim6, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  }, [fadeAnim5]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{paddingTop: 70}}>
+        <Animated.Text style={{fontSize: 34, opacity: fadeAnim5}}>Breathe in.</Animated.Text>
+        <Animated.Text style={{fontSize: 34, opacity: fadeAnim6}}>Breathe out.</Animated.Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const MindInformationPage = ({navigation}) => {
+
+}
 
 
 //START BODY PAGE
@@ -118,6 +227,8 @@ export default function App() {
         <Stack.Screen name="Mind" component={MindPage} />
         <Stack.Screen name="Body" component={BodyPage} />
         <Stack.Screen name="Soul" component={SoulPage} />
+        <Stack.Screen name="Breath" component={BreathingPage} />
+        <Stack.Screen name="Extra Information" component={MindInformationPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -136,8 +247,12 @@ const styles = StyleSheet.create({
     fontSize: 34,
     paddingTop: 70,
   },
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   buttonStyle: {
-    
     margin: 20,
     resizeMode: 'contain',
     //margin: 5,
