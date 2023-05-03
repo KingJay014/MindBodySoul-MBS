31412495fc6a4fd527817802cbe4485e505501de
grotaatwit <grota@wit.edu>
2023-05-03 16:57:52 -0400

    finished breathing section

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, Animated, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Animated, TouchableOpacity, Image, ScrollView } from 'react-native';
 import React, {useEffect, useRef} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -142,11 +142,23 @@ const MindPage = ({navigation}) => {
 }
 //END MIND PAGE
 
+
+
+//START BREATHING PAGE
 const BreathingPage = ({navigation}) => {
 
   const fadeAnim5 = useRef(new Animated.Value(0)).current;
   const fadeAnim6 = useRef(new Animated.Value(0)).current;
 
+  const fadeAnim7 = useRef(new Animated.Value(0)).current;
+  const fadeAnim8 = useRef(new Animated.Value(0)).current;
+  const fadeAnim9 = useRef(new Animated.Value(0)).current;
+
+  const fadeAnim10 = useRef(new Animated.Value(0)).current;
+  const fadeAnim11 = useRef(new Animated.Value(0)).current;
+  const fadeAnim12 = useRef(new Animated.Value(0)).current;
+
+  //46 BREATHING START
   useEffect(() => {
     Animated.loop(
       Animated.sequence([
@@ -186,22 +198,234 @@ const BreathingPage = ({navigation}) => {
         }),
       ]),
     ).start();
-  }, [fadeAnim5]);
+
+    //478 BREATHING START
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim7, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim7, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim7, {
+          toValue: 0,
+          duration: 15000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim8, {
+          toValue: 0,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim8, {
+          toValue: 1,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim8, {
+          toValue: 0,
+          duration: 3000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim8, {
+          toValue: 0,
+          duration: 8000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim9, {
+          toValue: 0,
+          duration: 11000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim9, {
+          toValue: 1,
+          duration: 5000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim9, {
+          toValue: 0,
+          duration: 3000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+
+    //BOX BREATHING START
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim10, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim10, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim10, {
+          toValue: 0,
+          duration: 12000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim11, {
+          toValue: 0,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim11, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim11, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim11, {
+          toValue: 0,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim11, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim11, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim12, {
+          toValue: 0,
+          duration: 8000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim12, {
+          toValue: 1,
+          duration: 2500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim12, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }),
+        Animated.timing(fadeAnim12, {
+          toValue: 0,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  }, [fadeAnim5, fadeAnim6, fadeAnim7, fadeAnim8, fadeAnim9, fadeAnim10, fadeAnim11, fadeAnim12]);
 
   return (
+    
     <SafeAreaView style={styles.container}>
-      <View style={{paddingTop: 70}}>
-        <Animated.Text style={{fontSize: 34, opacity: fadeAnim5}}>Breathe in.</Animated.Text>
-        <Animated.Text style={{fontSize: 34, opacity: fadeAnim6}}>Breathe out.</Animated.Text>
+    <ScrollView>
+      <View style={{paddingTop: 40}}>
+       
+        <Text style={styles.titletext}>
+          Breathing can{'\n'}
+          calm the mind.
+        </Text>
+
+        <Text style={styles.bodytext}>
+        Deep breathing is a simple, yet powerful technique that has 
+        been shown to have numerous benefits for the mind. Deep breathing 
+        can help with stress, anxiety, emotional regulation, focus, and calm
+        a chaotic mind.{'\n'}{'\n'}
+
+        Below, you can find various breathing patterns to help alleviate
+        whatever issue you may be having, or to simply help you relax.
+        </Text>
+
+        
+
+        <Text style={styles.titletextunderlined}>
+        "4:6" Method
+        </Text>
+
+        <View style={{padding: 50}}>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim5}}>Breathe in.</Animated.Text>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim6}}>Breathe out.</Animated.Text>
+        </View>
+
+        <Text style={styles.bodytext}>
+        Here, we inhale as much as possible, comfortably, for 4 seconds through the nose and 6 seconds out the mouth. 
+        This is a simple way to ground yourself, relieve stress and anxiety, and enter a calm state of mind.
+        </Text>
+
+        <Text style={styles.titletextunderlined}>
+        "4-7-8" Method
+        </Text>
+
+        <View style={{padding: 50}}>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim7}}>Breathe in.</Animated.Text>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim8}}>Hold.</Animated.Text>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim9}}>Breathe out.</Animated.Text>
+        </View>
+
+        <Text style={styles.bodytext}>
+        Here, we inhale with our nose for 4 seconds, hold the breath for 7 seconds,
+        and exhale out the mouth for 8 seconds. Use this breathing technique to decrease anxiety, sleep better, 
+        manage food cravings and control emotional responses like anger.
+        </Text>
+
+        <Text style={styles.titletextunderlined}>
+        "Box Breathing" Method
+        </Text>
+
+        <View style={{padding: 50}}>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim10}}>Breathe in.</Animated.Text>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim11}}>Hold.</Animated.Text>
+        <Animated.Text style={{textAlign: 'center', fontSize: 34, opacity: fadeAnim12}}>Breathe out.</Animated.Text>
+        </View>
+
+        <Text style={styles.bodytext}>
+        Here, we inhale with our nose for 4 seconds, hold the breath for 4 seconds,
+        exhale out the mouth for 4 seconds, and hold again for 4 more seconds. This breathing technique
+        , famous from its usage by the U.S. Navy Seals, helps reduce stress, anxiety, and fortifies focus.
+
+        </Text>
+
       </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
+//END BREATHING PAGE
 
+
+//START MIND INFO PAGE
 const MindInformationPage = ({navigation}) => {
 
 }
-
+//END MIND INFO PAGE
 
 //START BODY PAGE
 const BodyPage = ({navigation}) => {
@@ -245,7 +469,29 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 34,
-    paddingTop: 70,
+    paddingTop: 30,
+  },
+  titletext: {
+    fontSize: 34,
+    padding: 30,
+    textAlign: 'center',
+  },
+  titletextunderlined: {
+    fontSize: 34,
+    padding: 30,
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+  },
+  titletext2: {
+    fontSize: 30,
+    padding: 30,
+    textAlign: 'center',
+  },
+  bodytext: {
+    fontSize: 24,
+    marginHorizontal: 10,
+    textAlign: 'justify',
+    padding: 30,
   },
   centered: {
     flex: 1,
